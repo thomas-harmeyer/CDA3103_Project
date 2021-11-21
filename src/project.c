e05d5916016021e8e4fa73966aa3cc637bbdec04
Jebaited_U <zappiepo@aol.com>
2021-11-21 18:08:29 -0500

    Update project.c

--- src/project.c
+++ src/project.c
@@ -63,7 +63,14 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-
+    // use masking to get the bits we need for all the variables
+    *op = (instruction & 0xFC000000) >> 26;
+    *r1 = (instruction & 0x3E00000) >> 26;
+    *r2 = (instruction & 0x1F0000) >> 26;
+    *r3 = (instruction & 0xF800) >> 26;
+    *funct = (instruction & 0x3F);
+    *offset = (instruction & 0xFFFF);
+    *jsec = (instruction & 0x3FFFFFF);
 }
 
 
@@ -72,7 +79,80 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
+    // set initial values to 0
+    controls->ALUOp = 0;
+    controls->ALUSrc = 0;
+    controls->Branch = 0;
+    controls->Jump = 0;
+    controls->MemRead = 0;
+    controls->MemtoReg = 0;
+    controls->MemWrite = 0;
+    controls->RegDst = 0;
+    controls->RegWrite = 0;
 
+    //check op to get desired operation
+    if(op == 0x0){
+        // set up controls for r-type instructions
+        controls->ALUOp = 0x7;
+        controls->RegDst = 1;
+        controls->RegWrite = 1;
+    }
+    else if(op == 0x08){
+        // set up controls for add immediate
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+    }
+    else if(op == 0x23){
+        // set up controls for load word
+        controls->ALUSrc = 0x1;
+        controls->MemRead = 0x1;
+        controls->MemtoReg = 0x1;
+        controls->MemWrite = 0x1;
+    }
+    else if(op == 0x2B){
+        // set up controls for save word
+        controls->ALUSrc = 0x1;
+        controls->MemtoReg = 2;
+        controls->MemWrite = 0x1;
+        controls->RegDst = 2;
+    }
+    else if(op == 0x04){
+        // set up controls for beq
+        controls->ALUOp = 0x1;
+        controls->Branch = 1;
+        controls->MemtoReg = 2;
+        controls->RegDst = 2;
+    }
+    else if(op == 0x02){
+        // set up controls for jump operation
+        controls->ALUOp = 2;
+        controls->ALUSrc = 2;
+        controls->Jump = 1;
+        controls->MemtoReg = 2;
+        controls->RegDst = 2;
+    }
+    else if(op == 0x0A){
+        // set up controls for set on less than immediate
+        controls->ALUOp = 2;
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+    }
+    else if(op == 0x0B){
+        // sets the controls for sliu
+        controls->ALUOp = 3;
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+    }
+    else if(op == 0x0F){
+        // sets up the controls for lui
+        controls->ALUOp = 6;
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+    }
+    // if we get here, then we have an invalid command. halt the program.
+    else return 1;
+    // we have a valid command, return 0 to keep going
+    return 0;
 }
 
 /* Read Register */
